bd7acbec2a19cc52642de5a0f9320d3608598f80
Ryan Crichton <rg.crichton@gmail.com>
2015-03-02 13:12:44 +0200

    Allowed load test params to be passed to run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 ./mock-reg-server.js &
 pid=$!
 
-node_modules/.bin/loadtest -c 5 -t 20 -p reqGenerator.js -T 'multipart/related; boundary=MIMEBoundaryurn_uuid_DCD262C64C22DB97351256303951323; type="application/xop+xml"; start="<0.urn:uuid:DCD262C64C22DB97351256303951324@apache.org>"; start-info="application/soap+xml"; action="urn:ihe:iti:2007:ProvideAndRegisterDocumentSet-b"' http://localhost:8081/openmrs/ms/xdsrepository
+if [ $# -gt 0 ]; then
+	echo "Passing "$*" to loadtest..."
+fi
+
+node_modules/.bin/loadtest $* -p reqGenerator.js -T 'multipart/related; boundary=MIMEBoundaryurn_uuid_DCD262C64C22DB97351256303951323; type="application/xop+xml"; start="<0.urn:uuid:DCD262C64C22DB97351256303951324@apache.org>"; start-info="application/soap+xml"; action="urn:ihe:iti:2007:ProvideAndRegisterDocumentSet-b"' http://localhost:8081/openmrs/ms/xdsrepository
 
 kill $pid
